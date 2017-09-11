301c0f6802e57d4b66c6e759bb518b7ba6ea2d55
Chris Turina and Dan Bixel <chris+dan@>
2017-09-11 16:27:08 -0700

    javascript content

---
+++ Cipher/js/scripts.js
@@ -0,0 +1,4 @@
+var sentence1 = prompt("Where is your favorite place?");
+var first = sentence1.charAt(0).toUpperCase();
+var last = sentence1.charAt(sentence1.length-1).toUpperCase();
+  alert(first + last)
